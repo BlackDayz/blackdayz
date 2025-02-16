da1402f5b0e9f6a06b179204d572ed36fa247e48
Ben <67608550+Mittelblut9@users.noreply.github.com>
2025-02-16 12:02:47 +0100

    Initial commit

---
+++ src/components/utils/footer.component.vue
@@ -0,0 +1,39 @@
+<template>
+    <UFooter :links="links">
+        <template #left>
+            <span
+                v-text="useI18n().t('footer.copyright', {
+                    year: new Date().getFullYear(),
+                })"
+            />
+        </template>
+
+        <template #right>
+            <UColorModeButton />
+            <UButton
+                icon="i-simple-icons-github"
+                color="gray"
+                variant="ghost"
+                to="https://github.com/Mittelblut9"
+                target="_blank"
+            />
+        </template>
+    </UFooter>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'FooterComponent',
+    data() {
+        return {
+            links: [
+                {
+                    label: useI18n().t('header.links.example.label'),
+                    icon: useI18n().t('header.links.example.icon'),
+                    to: useI18n().t('header.links.example.to')
+                },
+            ]
+        };
+    }
+};
+</script>
---
+++ src/components/utils/header.component.vue
@@ -0,0 +1,24 @@
+<template>
+    <UHeader :links="links">
+        <template #panel>
+            <UNavigationTree :links="links" />
+        </template>
+    </UHeader>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'HeaderComponent',
+    data() {
+        return {
+            links: [
+                {
+                    label: useI18n().t('header.links.example.label'),
+                    icon: useI18n().t('header.links.example.icon'),
+                    to: useI18n().t('header.links.example.to')
+                },
+            ]
+        };
+    }
+};
+</script>
