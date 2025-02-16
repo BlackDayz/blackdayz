06c831927975b0eff87d9d5349b33f360afe1129
Mittelblut9 <sauer.benedikt@outlook.de>
2025-02-17 00:13:34 +0100

    feat: restructure index page layout with new atom components and update page name to HomePage

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,32 @@
 <template>
-    <h1>{{ useI18n().t('hello.world') }}</h1>
+    <div>
+        <div class="grid lg:flex items-center h-[37vh]">
+            <div class="lg:w-1/2 grid gap-6">
+                <H1Atom :text="text" />
+                <PAtom :text="text" />
+                <div class="grid w-full sm:flex gap-4 mt-9 text-center">
+                    <PrimaryButtonAtom text="Button" @on-click="console.log('es')" />
+                    <PrimaryButtonAtom text="Button" @on-click="console.log('es')" />
+                </div>
+            </div>
+            <div class="lg:w-1/2">
+                <ImageAtom src="https://i.ytimg.com/vi/KSpwlGWD51s/maxresdefault.jpg" alt="Image" />
+            </div>
+        </div>
+        <div class="h-56 grid items-center">
+            <SpacerLinesAtom />
+        </div>
+    </div>
 </template>
 
-<script lang="ts">
+<script>
 export default {
-    name: 'IndexPage',
+    name: 'HomePage',
     data() {
-        return {};
+        return {
+            text: 'Lorem ipsum dolor sit amet'
+        }
     },
-};
+
+}
 </script>
