3cca2fafbf725aadd1b34a5fd0337f8d2988d863
Mittelblut9 <sauer.benedikt@outlook.de>
2025-02-17 12:28:56 +0100

    feat: add ClientsShowcaseAtom and TrustedByComponent for client display

---
+++ src/components/atoms/clients-showcase.atom.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex flex-wrap justify-center gap-16">
+        <div
+            v-for="client in clients"
+            :key="client.name"
+            class="grid gap-2"
+        >
+            <NuxtImg
+                :src="client.image"
+                :alt="client.name"
+                width="200px"
+                class="border-none rounded-none"
+            />
+            <p class="text-center font-bold text-lg text-gray-900">
+                {{ client.name }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ClientsShowcaseAtom',
+  props: {
+    clients: {
+      type: Array as PropType<ClientShowcaseInterface[]>,
+      required: true
+    }
+  }
+};
+</script>
---
+++ src/components/trusted-by.component.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="grid justify-center text-center gap-20">
+        <H2Atom text="Trusted By" />
+        <ClientsShowcaseAtom
+            :clients="[
+                {
+                    image: 'https://i.ytimg.com/vi/KSpwlGWD51s/maxresdefault.jpg',
+                    name: 'Rumathra',
+                },
+                {
+                    image: 'https://i.ytimg.com/vi/KSpwlGWD51s/maxresdefault.jpg',
+                    name: 'htnh',
+                },
+                {
+                    image: 'https://i.ytimg.com/vi/KSpwlGWD51s/maxresdefault.jpg',
+                    name: 'Rumathra',
+                },
+
+                {
+                    image: 'https://i.ytimg.com/vi/KSpwlGWD51s/maxresdefault.jpg',
+                    name: 'Rumathra',
+                },
+
+                {
+                    image: 'https://i.ytimg.com/vi/KSpwlGWD51s/maxresdefault.jpg',
+                    name: 'Rumathra',
+                },
+
+                {
+                    image: 'https://i.ytimg.com/vi/KSpwlGWD51s/maxresdefault.jpg',
+                    name: 'Rumathra',
+                },
+            ]"
+        />
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'TrustedByComponent',
+};
+</script>
