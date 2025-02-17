e7134debba21ba4fb28d626680f52f70e3d26135
Mittelblut9 <sauer.benedikt@outlook.de>
2025-02-17 12:29:22 +0100

    feat: create H2Atom component for enhanced heading styling with word separation

---
+++ src/components/atoms/utils/typography/h2.atom.vue
@@ -0,0 +1,30 @@
+<template>
+    <h2>
+        <span
+            v-for="(word, index) in words"
+            :key="index"
+            class="relative inline-block px-1"
+            :class="{ 'px-3': (index !== 0 && index !== words.length - 1)|| words.length <= 2 }"
+        >
+            <span class="absolute left-0 bottom-1 h-12 w-full bg-primary-500 -z-10 translate-y-6" />
+            {{ word }}
+        </span>
+    </h2>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'H2Atom',
+  props: {
+      text: {
+          type: String,
+          required: true,
+      },
+  },
+  computed: {
+      words() {
+          return this.text.split(' ');
+      }
+  }
+};
+</script>
