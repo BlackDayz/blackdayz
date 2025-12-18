c6cb172a90629ff0dc7d17753bfbe19670ce4bb0
Mittelblut9 <sauer.benedikt@outlook.de>
2025-12-18 17:18:00 +0100

    feat: add Datenschutzerklärung and Impressum pages for legal compliance

---
+++ app/pages/impressum.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1 class="text-5xl text-center mb-10 font-bold">
+            Impressum
+        </h1>
+        <p>
+            BlackDayz<br>
+            Benedikt Sauer<br>
+            Obertorstra&szlig;e 12<br>
+            53340 Meckenheim
+        </p>
+        <br>
+        <h4 class="text-3xl mb-1 mt-5 font-bold">
+            Kontakt
+        </h4>
+        <p>
+            Telefon: +49 15110842428<br>
+            E-Mail: info[at]blackdayz.de
+        </p>
+        <h4 class="text-3xl mb-1 mt-5 font-bold">
+            EU-Streitschlichtung
+        </h4>
+        <p>
+            Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a
+                href="https://ec.europa.eu/consumers/odr/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >https://ec.europa.eu/consumers/odr/</a>.<br> Unsere E-Mail-Adresse finden Sie oben im Impressum.
+        </p>
+
+        <h4 class="text-3xl mb-1 mt-5 font-bold">
+            Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle
+        </h4>
+        <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+
+        <p>Quelle: <a href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a></p>
+
+        <h4 class="text-3xl mb-1 mt-5 font-bold">
+            Hinweis gemäß Verbraucherstreitbeilegungsgesetz (VSBG)
+        </h4>
+        <p>Wir sind nicht bereit und verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+    </div>
+</template>
