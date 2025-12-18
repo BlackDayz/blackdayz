4a63e112467d14dad58725cf427a294628768e10
Mittelblut9 <sauer.benedikt@outlook.de>
2025-12-18 13:07:34 +0100

    feat: update contact component layout for improved styling

--- app/components/contact.component.vue
+++ app/components/contact.component.vue
@@ -1,16 +1,20 @@
 <template>
-    <div class="mb-14 flex justify-center">
-        <H4Atom :text="useT('contact.title')" />
-    </div>
-    <div>
-        <p>{{ useT('contact.description') }}</p>
-    </div>
-    <div class="flex justify-center mt-5">
-        <SecondaryButtonAtom
-            icon="mdi:email"
-            :text="useT('contact.button.email')"
-            class="flex py-3"
-            :href="'mailto:' + useT('contact.email')"
-        />
+    <div class="mx-2 lg:mx-0">
+        <div class="mb-14 flex justify-center">
+            <H4Atom :text="useT('contact.title')" />
+        </div>
+        <div>
+            <p class="text-center">
+                {{ useT('contact.description') }}
+            </p>
+        </div>
+        <div class="flex justify-center mt-5">
+            <SecondaryButtonAtom
+                icon="mdi:email"
+                :text="useT('contact.button.email')"
+                class="flex py-3"
+                :href="'mailto:' + useT('contact.email')"
+            />
+        </div>
     </div>
 </template>
