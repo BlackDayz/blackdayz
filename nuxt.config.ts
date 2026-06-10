162ef743faf988056135543e0200c878f117ba68
Mittelblut9 <sauer.benedikt@outlook.de>
2026-06-10 16:56:56 +0200

    fix: geneate i18n json file instant

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 import { generateJsonTranslations } from './scripts/i18n/loadYamlTranslations';
 
+generateJsonTranslations();
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
